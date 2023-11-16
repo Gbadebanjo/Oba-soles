df21338cf90ca0233be5bd91e6b025e1877e71a2
oluwagbogo <oluwagbogoadebanjo@gmail.com>
2023-11-16 23:30:07 +0100

    Section 2 progress

--- src/Components/home/SectionTwo.jsx
+++ src/Components/home/SectionTwo.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPercent } from "@fortawesome/free-solid-svg-icons";
+import Cart from "../../Image/browncart.png";
+import Seed from "../../Image/icons8-seed-96.png";
 
 const Container = styled.div`
   height: 50%;
@@ -13,53 +15,60 @@ const Container = styled.div`
   align-items: center;
 `;
 
+
+
 const Box = styled.div`
   height: 250px;
-  padding-right: ;
   width: 25%;
-  color: #fff;
+  
   margin: 20px;
   border-radius: 10px;
   box-shadow: 0px -1px 15px -5px #a8a4a4;
   -webkit-box-shadow: 0px -1px 15px -5px #dad0d0;
   border-left: 5px solid #823f27;
-  background: linear-gradient(to right, #fff 50%, #823f27 50%);
   background-size: 200% 100%;
   background-position: right bottom;
   transition: all 1.2s ease-out;
+  color: #823f27;
+  background-color: #fff;
   &:hover {
     background-position: left bottom;
     transform: scale(1.2);
     transition: all 350ms;
     cursor: pointer;
-    color: #823f27;
+    color: #fff;
+    background-color: #823f27;
   }
 `;
 
-const BoxImg = styled.img`
-  height: 20px;
-  width: 20px;
-  border-radius: 10px; // make the image round
-  object-fit: cover; // resize the image to fit its container while maintaining its aspect ratio
-  margin: 5px; // add some space around the image
-  transition: transform 0.2s; // animate any changes to the transform property
+const BoxImg = styled.div`
+  font-size: 70px;
+  margin-left: 30px;
+  font-weight: bold;
+  background-position: right bottom;
 
-  &:hover {
-    transform: scale(1.1); // scale up the image when it's hovered over
+  img {
+    filter: sepia(1); // make the image brown
+  }
+
+  &:hover img {
+    filter: invert(1); // make the image white on hover
   }
 `;
 
 const BoxText = styled.p`
   font-size: 20px;
   font-weight: 600;
-  padding-left: 10px;
-  padding-top: 10px;
+  margin-left: 30px;
+//   color: #fff;
+
 `;
 
 const BoxLink = styled.a`
 text-decoration: none;
 font-size: 20px;
 font-weight: 600;
+margin-left: 30px;
 
 `
 
@@ -67,17 +76,17 @@ const SectionTwo = () => {
   return (
     <Container>
       <Box>
-      <FontAwesomeIcon icon={faPercent}  size="3x" size="3x" style={{fontWeight: "bold"}}/>
+      <BoxImg>%</BoxImg>
         <BoxText>Get Up To 5% Discount</BoxText>
         <BoxLink href="/product">Shop Now</BoxLink>
       </Box>
       <Box>
-      <FontAwesomeIcon icon={faPercent}  size="3x" size="3x" style={{fontWeight: "bold"}}/>
+        <BoxImg><img src={Cart} alt="cart"/></BoxImg>
         <BoxText>New Arrivals</BoxText>
         <BoxLink href="/product">Shop Now</BoxLink>
       </Box>
       <Box>
-      <FontAwesomeIcon icon={faPercent}  size="3x" size="3x" style={{fontWeight: "bold"}}/>
+      <BoxImg><img src={Seed} alt="seed"/></BoxImg>
         <BoxText>Best Selling Products</BoxText>
         <BoxLink href="/product">Shop Now</BoxLink>
       </Box>

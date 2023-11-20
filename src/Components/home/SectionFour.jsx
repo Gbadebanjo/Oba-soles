396eca4b2132de6e24a3533e99da326afefa4439
oluwagbogo <oluwagbogoadebanjo@gmail.com>
2023-11-20 20:06:12 +0100

    Added sectionfour

--- src/Components/home/SectionFour.jsx
+++ src/Components/home/SectionFour.jsx
@@ -1,14 +1,42 @@
 import styled from "styled-components";
 const Container = styled.div`
-  height: 70vh;
+  height: 40%;
   width: 100%;
   display: flex;
   flex-wrap: wrap;
   background-color: red;
 `;
 
+const Description = styled.h1`
+display: center;
+text-align: center;
+font-size: 3rem;
+font-weight: 700;
+// font-family: "Roboto", sans-serif;
+color: black;
+`;
+
+const Box = styled.div`
+  height: 100%;
+  width: 25%;
+  background-color: blue;
+  &:hover {
+    background-color: yellow;
+  } ;
+`;
+
 const SectionFour = () => {
-  return <Container>SectionFour</Container>;
+  return (
+    <>
+    <Description>New Arrivals</Description>
+    <Container>
+      <Box>ALl</Box>
+      <Box>ALl</Box>
+      <Box>ALl</Box>
+      <Box>ALl</Box>
+    </Container>
+  </>
+    );
 };
 
 export default SectionFour;
--- src/Components/home/SectionThree.jsx
+++ src/Components/home/SectionThree.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import ObaImg from "../../Image/ObaDesign2.jpg";
 
 const Container = styled.div`
-  height: 70vh;
+  height: 50vh;
   width: 100%;
   display: flex;
   flex-wrap: wrap;
@@ -12,20 +12,20 @@ const Description = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  padding: 100px;
+  padding: 10px;
   float: left;
   box-sizing: border-box;
 `;
 
 const Text = styled.h1`
   color: red;
-  font-size: 2rem;
+  font-size: 3rem;
   font-weight: 1000;
   text-align: center;
 `;
 
 const DesignBox = styled.div`
-  height: 70vh;
+  height: 50vh;
   width: 50%;
 
   display: flex;

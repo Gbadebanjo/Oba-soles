55b7463872323c158fd1ca3d2160d9e5939098fe
oluwagbogo <oluwagbogoadebanjo@gmail.com>
2023-11-25 23:54:44 +0100

    House keeping

--- src/Components/home/Footer.jsx
+++ src/Components/home/Footer.jsx
@@ -1,13 +1,26 @@
 import styled from "styled-components";
 
-const Container = styled.div`
-height: 50%;
-weight: 100%;
+const Container1 = styled.div`
+height: 50px;
+weight: 50px;
 background-color: blue;
+display: flex;
+justify-content: center;
+align-items: center;
+// padding: 10px;
+margin: 460px;
+border-radius: 10px;
+`;
+
+const Container2 = styled.div`
+height: 50%;
+weight: 50%;
+background-color: red;
 `;
 const Footer = () => {
   return (
-    <Container>Footer</Container>
+    <Container1>Footer</Container1>
+    // <Container2>Footer</Container2>
   )
 }
 

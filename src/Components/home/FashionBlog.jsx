18cceef741fed49f5577df44e04b03a60628a0e7
oluwagbogo <oluwagbogoadebanjo@gmail.com>
2023-12-06 20:16:26 +0100

    Fashionblog

--- src/Components/home/FashionBlog.jsx
+++ src/Components/home/FashionBlog.jsx
@@ -1,19 +1,58 @@
 import styled from "styled-components";
 import { Description } from "./SectionFour";
+
 const Container = styled.div`
-  height: 50%;
+  height: 70%;
   weight: 100%;
-  background-color: green;
-`;
+  background-color: #e3f7ff;
+  `;
 
 const Wrapper = styled.div`
-  height: 50%;
-  weight: 100%;
+  height: 70%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #004159;
 `;
+
+const CardContainer = styled.div`
+height: 100%;
+width: 100%;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`;
+
+const Img = styled.img`
+height: 400px;
+weight:
+`;
+
+const Title = styled.h1`
+font-size: 2rem;
+font-weight: 700;
+color: white;
+font-family: 'Roboto', sans-serif;
+`;
+
+
+ 
 const FashionBlog = () => {
   return (
     <Container>
       <Description>Read Fashion Tips</Description>
+      <Wrapper>
+        <CardContainer>
+          <Title>How to wear white sneakers</Title>
+        </CardContainer>
+        <CardContainer>
+          <Title>How to wear white sneakers</Title>
+        </CardContainer>
+        <CardContainer>
+          <Title>How to wear white sneakers</Title>
+        </CardContainer>
+      </Wrapper>
     </Container>
   );
 };

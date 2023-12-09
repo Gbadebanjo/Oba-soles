1ce385e11bf982e5ec49679e4c912c34a6a3bb97
oluwagbogo <oluwagbogoadebanjo@gmail.com>
2023-12-09 23:42:14 +0100

    Added to fashionBlog

--- src/Components/home/FashionBlog.jsx
+++ src/Components/home/FashionBlog.jsx
@@ -1,14 +1,15 @@
 import styled from "styled-components";
-
+import CleanShoe from "../../Image/istockphoto-1023542632-612x612.jpg";
+import { Link } from "react-router-dom";
 const Container = styled.div`
-  height: 70%;
+  height: auto;
   weight: 100%;
   background-color: #e3f7ff;
   padding: 0px;
   margin-top: 0px;
-  `;
+`;
 
-  const Description = styled.h1`
+const Description = styled.h1`
   display: center;
   text-align: center;
   font-size: 3rem;
@@ -22,75 +23,92 @@ const Container = styled.div`
     font-size: 2.5rem;
     margin: 0px;
   }
-  `;
+`;
 
 const Wrapper = styled.div`
-  height: 90%;
+  height: 80%;
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-around;
-  align-items: center;
-  background-color: #004159;
 `;
 
 const CardContainer = styled.div`
-height: 100%;
-width: 100%;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
+  height: 550px;
+  width: 450px;
+  display: flex;
+  flex-direction: column;
+  align-text: start;
 `;
 
 const CardImg = styled.img`
-height: 550px;
-weight: 500px;
-object-fit: cover;
+  height: 280px;
+  weight: 50px;
+  object-fit: cover;
+  border-radius: 5px;
 `;
 
 const Title = styled.h1`
-font-size: 2rem;
-font-weight: 700;
-color: white;
-font-family: 'Roboto', sans-serif;
+  font-size: 1.5rem;
+  font-weight: 800;
+  color: #5c5c5c;
+  paddding: 0px;
+  margin: 0px;
 `;
 
 const Date = styled.p`
-font-size: 1rem;
-font-weight: 700;
-color: white;
-font-family: 'Roboto', sans-serif;
+  font-size: 1rem;
+  font-weight: 600;
+  color: #5c5c5c;
+  font-family: "Roboto", sans-serif;
+  padding: 0px;
+  margin: 0px;
 `;
 
 const Text = styled.p`
-font-size: 1rem;
-font-weight: 700;
-color: white;
-font-family: 'Roboto', sans-serif;
+  font-size: 1rem;
+  font-weight: 700;
+  color: black;
+  font-family: "Roboto", sans-serif;
 `;
 
- 
 const FashionBlog = () => {
   return (
     <Container>
       <Description>Read Fashion Tips</Description>
       <Wrapper>
         <CardContainer>
-          <CardImg src="https://images.unsplash.com/photo-1581093458794-0c6b3b3d0a9c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2Vua2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" />
+          <CardImg src={CleanShoe} />
           <Title>How to wear white sneakers</Title>
           <Date>March 15, 2021</Date>
-          <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ut! Cupiditate dolorum qui, dolor veniam dolores, repudiandae debitis vero quas consequuntur recusandae nulla modi consectetur obcaecati ratione inventore nostrum ullam.</Text>
+          <Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ut!
+            Cupiditate dolorum qui, dolor veniam dolores, repudiandae debitis
+            vero quas consequuntur recusandae nulla modi consectetur obcaecati
+            ratione inventore nostrum ullam.
+          </Text>
+          <Link to="/blog">Read More</Link>
         </CardContainer>
         <CardContainer>
-          <CardImg src="https://images.unsplash.com/photo-1581093458794-0c6b3b3d0a9c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2Vua2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" />
+          <CardImg src={CleanShoe} />
           <Title>How to wear white sneakers</Title>
           <Date>March 15, 2021</Date>
-          <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ut! Cupiditate dolorum qui, dolor veniam dolores, repudiandae debitis vero quas consequuntur recusandae nulla modi consectetur obcaecati ratione inventore nostrum ullam.</Text>
+          <Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ut!
+            Cupiditate dolorum qui, dolor veniam dolores, repudiandae debitis
+            vero quas consequuntur recusandae nulla modi consectetur obcaecati
+            ratione inventore nostrum ullam.
+          </Text>
         </CardContainer>
         <CardContainer>
-          <CardImg src="https://images.unsplash.com/photo-1581093458794-0c6b3b3d0a9c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2Vua2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" />
+          <CardImg src={CleanShoe} />
           <Title>How to wear white sneakers</Title>
           <Date>March 15, 2021</Date>
-          <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ut! Cupiditate dolorum qui, dolor veniam dolores, repudiandae debitis vero quas consequuntur recusandae nulla modi consectetur obcaecati ratione inventore nostrum ullam.</Text>
+          <Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ut!
+            Cupiditate dolorum qui, dolor veniam dolores, repudiandae debitis
+            vero quas consequuntur recusandae nulla modi consectetur obcaecati
+            ratione inventore nostrum ullam.
+          </Text>
         </CardContainer>
       </Wrapper>
     </Container>

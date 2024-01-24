a155aab443221268c3f59096b80a8e52584b1e45
oluwagbogo <oluwagbogoadebanjo@gmail.com>
2024-01-24 01:08:25 +0100

    Product updated

--- src/Components/product/Product.jsx
+++ src/Components/product/Product.jsx
@@ -3,21 +3,79 @@ import styled from "styled-components";
 import axios from "axios";
 
 const Container = styled.div`
-background-color: red;
+display: flex;
+flex-wrap: wrap;
+justify-content: space-evenly;
 height: 100vh;
+padding-top: 7%;
 `;
 
+const ProductBox = styled.div`
+height: 350px;
+width: 300px;
+margin: 0% 1%;
+justify-content: center;
+align-items: center;
+-webkit-box-shadow: 0px -1px 15px -5px #dad0d0;
+box-shadow: 0px -1px 15px -5px #a8a4a4;
+margin-bottom: 10%;
 
-function ProductPage() {
+@media only screen and (max-width: 768px) {
+  height: 350px;
+  width: 330px;
+  padding-bottom: 20%;
+  -webkit-box-shadow: none;
+  margin-bottom: 1%;
+
+}
+`;
+
+const ProductImage = styled.img`
+height: 100%;
+width: 100%;
+object-fit: cover;
+transition: all 350ms;
+border-radius: 7px;
+&:hover {
+  opacity: 0.7;
+  cursor: pointer;
+}
+`;
+
+const ProductName = styled.h3`
+color: black;
+font-size: 1.2rem;
+font-weight: 700;
+display: flex;
+justify-content: center;
+text-align: center;
+align-items: center;
+margin: 0;
+padding: 0;
+`;
+
+const ProductPrice = styled.p`
+color: black;
+font-size: 1.2rem;
+font-weight: 700;
+display: flex;
+justify-content: center;
+text-align: center;
+align-items: center;
+margin: 0;
+padding: 0;
+`;
+
+const ProductPage = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get('/products');
+        const response = await axios.get("/products");
         setProducts(response.data);
       } catch (error) {
-        console.error('Error fetching products:', error);
+        console.error("Error fetching products:", error);
       }
     };
 
@@ -25,16 +83,16 @@ function ProductPage() {
   }, []);
 
   return (
-    <div>
+    <Container>
       {products.map((product) => (
-        <div key={product._id} onClick={() => alert('Show more details')}>
-          <img src={product.picture} alt={product.name} />
-          <h2>{product.name}</h2>
-          <p>{product.price}</p>
-        </div>
+        <ProductBox key={product._id}>
+          <ProductImage src={product.picture} alt={product.name} />
+          <ProductName>{product.name}</ProductName>
+          <ProductPrice>{product.price}</ProductPrice>
+        </ProductBox>
       ))}
-    </div>
+    </Container>
   );
-}
+};
 
 export default ProductPage;

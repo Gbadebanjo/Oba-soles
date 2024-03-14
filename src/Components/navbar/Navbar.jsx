5488e347cbfaa97671077052ece3af63b7aa432d
oluwagbogo <oluwagbogoadebanjo@gmail.com>
2024-03-14 02:41:07 +0100

    Nav updated

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,159 +1,190 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
-import { NavLink, Link, Navigate } from "react-router-dom";
+import { NavLink, Navigate } from "react-router-dom";
 import UserContext from "../Context/UserContext";
 import React from "react";
 import Modal from "react-modal";
-import { MdClose, MdHome, MdEdit } from "react-icons/md";
 import { BiLogOut } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 import ObaLogo from "../../Image/OBA SOLES 1.png";
-const NavContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1rem 1rem;
-  background-color: #232323;
-  position: fixed;
-  width: 100%;
-  height: 50px;
-  opacity: 0.9; // to make the navbar semi-transparent
-  transition: opacity 0.3s ease; // animate the opacity change
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaAngleDown } from 'react-icons/fa';
+import { FaShoppingCart } from 'react-icons/fa';
 
-  &:hover {
-    opacity: 0.5; // make the navbar fully visible when the mouse hovers over it
-  }
-`;
-
-const BrandName = styled.h1`
-  font-size: 2.5rem;
-  font-weight: 700;
-  font-family: "Lucida Calligraphy", "Poppins", sans-serif;
-  letter-spacing: -0.2rem;
-  color: #ee0000;
-  text-shadow: 0.1rem 0.1rem 0.1rem #000;
-  padding: 0 1rem;
-  margin-left: 1%;
-  padding-top: 30px;
-`;
-
-const Sole = styled.span`
-  color: #fff;
-  font-weight: 700;
-  // font-size: 2.5rem;
-`;
-
-const NavLinks = styled.ul`
-  display: flex;
-  list-style: none;
-`;
-
-const NavItems = styled(NavLink)`
-  text-decoration: none;
-  color: #efefef;
-  font-size: 1.2rem;
-  font-weight: 500;
-  margin-left: 1.5rem;
-  padding-top: 20px;
-
-  &:hover {
-    color: #ee0000;
-    cursor: pointer;
-  }
-  &.active {
-    color: #ee0000;
-  }
-`;
-
-const CartContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding-top: 10px;
-  color: #efefef;
-  &:hover {
-    color: ee0000#;
-    cursor: pointer;
-  }
-`;
-
-const CartCount = styled.span`
-  margin-left: 10px;
-  color: #fff;
-  margin-right: 40px;
-  margin-bottom: 10px;
-  font-size: 1.3rem;
-`;
-
-const UserImg = styled.div`
-  display: flex;
-  padding-right: 35px;
-  // margin-top: 10px;
-  svg {
-    width: 32px;
-    height: 32px;
-    fill: #fff;
-    &:hover {
-      fill: #ee0000;
-    }
-  }
-`;
-
-const UserName = styled.p`
-  color: #fff;
-  padding-left: 5px;
-  padding-top: 2px;
-  font-weight: 700;
-  text-decoration: none;
-  font-size: 1.1rem;
-`;
-
-const BreakPoint = styled.hr`
-  border: 0;
-  height: 1px;
-  background: red;
-`;
-
-const Search = styled.input`
-  height: 30px;
-  border-radius: 8px;
-  border: none;
-  padding: 10px;
-  font-size: 1rem;
-`;
-
-const SpanText = styled.span`
-  color: #ee0000;
-  font-weight: 400;
-  font-size: 18px;
-  padding-left: 20px;
-  padding-buttom: 5px;
+const fadeIn = keyframes`
+  // from { opacity: 0; transform: translateY(-20px); }
+  // to { opacity: 1; transform: translateY(0); }
+  from { transform: translateX(-100%); }
+  to { transform: translateX(0); }
 `;
 
 const NewContainer = styled.div`
   width: 100%;
-  height: 15vh;
+  height: 13vh;
   background-color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
   `;
 
   const NavLogo = styled.div`
-  width: 20%;
+  width: 15%;
   height: 100%;
+  display: flex;
+  justify-content: center;
+  animation: ${fadeIn} 0.8s ease forwards;
+  animation-delay: 0.2s;
   `;
 
   const Logo = styled.img`
   width: 100%;
-  height: 100%;
+  height: 90%;
   object-fit: contain;
+  margin-top: 3px;
   `;
+
+  const NavLinkSerachBox = styled.div`
+  width: 60%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  `;
+
+  const NavLinks = styled.div`
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+  width: 70%;
+`;
+
+const NavItems = styled(NavLink)`
+  text-decoration: none;
+  color: #000;
+  font-family: "Poppins", sans-serif;
+  font-size: 16px;
+  font-weight: 700;
+  animation: ${fadeIn} 0.10s ease forwards;
+  animation-delay: ${props => props.index * 0.3}s;
+
+
+  &:hover {
+    cursor: pointer;
+    text-decoration: underline;
+    text-decoration-color: #C39F51;
+  }
+  &.active {
+    color: #C39F51;
+    text-decoration-color: #C39F51;
+    text-decoration: underline;
+  }
+`;
+
+const SearchBox = styled.div`
+  width: 25%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  animation: ${fadeIn} 0.5s ease forwards;
+  animation-delay: 0.4s;
+  `;
+
+  const NewSearch = styled.input`
+  height: 35%;
+  width: 100%;
+  border-radius: 8px;
+  padding-left: 32px;
+  font-size: 1rem;
+`;
+
+const StyledSearchIcon = styled(AiOutlineSearch)`
+  position: absolute;
+  height: 25px;
+  width: 25px;
+  left: 8px;
+  top: 38%;
+`;
+
+const AccountCart = styled.div`
+  width: 15%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  `;
+
+const AccountInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  border-right: 1px solid #000;
+  padding-right: 10px;
+  gap: 5px;
+  height: 50%;
+
+  &:hover {
+    cursor: pointer;
+  }
+  `;
+
+  const AccountText = styled.p`
+  font-size: 16px;
+  font-weight: 700;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  margin: 0;
+  padding-bottom: 5px;
+  `;
+
+  const CartBox = styled.div`
+  width: 25%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  `;
+
+  const StyledFaShoppingCart = styled(FaShoppingCart)`
+  height: 25px;
+  width: 25px;
+  `;
+
+  const Count = styled.span`
+  position: absolute;
+  top: 10%;
+  right: 0;
+  background-color: #C39F51;
+  color: #fff;
+  border-radius: 50%;
+  padding: 7px;
+  font-size: 13px;
+  height: 10px;
+  width: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  `;
+
+
+
+
+
 
 const Navbar = () => {
   const navigate = useNavigate();
   const [cartCount, setCartCount] = useState(0);
   const { name, setName } = React.useContext(UserContext);
   const [isModalOpen, setIsModalOpen] = useState(false);
+
 
   function openModal() {
     if (name) {
@@ -192,8 +223,36 @@ const Navbar = () => {
     <NavLogo>
       <Logo src={ObaLogo} alt="Oba Logo" />
     </NavLogo>
+    <NavLinkSerachBox><NavLinks>
+          <NavItems to="/" exact>
+            Home
+          </NavItems>
+          <NavItems index={0} to="/product">Product</NavItems>
+          <NavItems index={1}to="/about">About</NavItems>
+          <NavItems index={2}to="/contact">Contact</NavItems>
+          <NavItems index={3}to="/blog">Blog</NavItems>
+        
+        </NavLinks>
+          <SearchBox>
+          <NewSearch type="search" />
+          <StyledSearchIcon />
+          </SearchBox></NavLinkSerachBox>
+    
+          <AccountCart>
+          <AccountInfo>
+            <AccountText>My Account</AccountText>
+            <FaAngleDown />
+          </AccountInfo>
+          <CartBox>
+          <StyledFaShoppingCart/>
+          <Count>{cartCount}</Count>
+          </CartBox>
+          </AccountCart>
     </NewContainer>
-      <NavContainer>
+
+
+
+      {/* <NavContainer>
         <BrandName>
           Oba<Sole>Soles</Sole>
         </BrandName>
@@ -272,7 +331,7 @@ const Navbar = () => {
           <BiLogOut size={24} color="#ee0000" />
           <SpanText>Logout</SpanText>
         </div>
-      </Modal>
+      </Modal> */}
     </>
   );
 };

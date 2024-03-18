80af721ae86830ce16b8dfdda4220a2abdb711d5
oluwagbogo <oluwagbogoadebanjo@gmail.com>
2024-03-18 23:52:54 +0100

    Sec2 update

--- src/Components/home/SectionOne.jsx
+++ src/Components/home/SectionOne.jsx
@@ -1,57 +1,115 @@
 import React from "react";
-import styled from "styled-components";
-import Shoe1 from "../../Image/Brouge.jpg";
+// import styled from "styled-components";
+import Shoe1 from "../../Image/pair-brown.png";
+import styled, { keyframes } from 'styled-components';
+import SalesImg from "../../Image/tag label.png";
+
+const slide = keyframes`
+  0% { background-position: 0% 50%; }
+  50% { background-position: 100% 50%; }
+  100% { background-position: 0% 50%; }
+`;
 
 const Container = styled.div`
   width: 100%;
-  height: 500px;
+  height: 550px;
   background-image: url(${Shoe1});
-  background-size: cover;
+  background-size: 110% auto;
   background-position: 25% ;
   background-attachment: fixed;
+  animation: ${slide} 30s linear infinite;
+  display: flex;
+  justify-content: end;
+  align-items: flex-end;
+  
+`;
+
+
+const Story = styled.div`
+// text-shadow: 0 0 20px #000;
+background-color: rgba(226,220,218, 0.9);
+border-radius: 40px 0 0 40px ;
+width: 35%;
+height: 80%;
+padding: 20px 0 0 60px;
+display: flex;
+flex-direction: column;
+
+@media screen and (max-width: 768px) {
+
+}
+`;
+
+const StoryHeading = styled.div`
+width: 100%;
+height: 50%;
+// background-color: blue;
 `;
 
 const Discount = styled.h1`
-  color: red;
-  font-size: 25px;
-  padding-top: 120px;
-  padding-left: 70px;
-  line-height: 1.1;
-  letter-spacing: 0.9px;
+  color: ${props => props.color || "#000"};
+  font-size: 130px;
+  font-family: Poppins;
+  font-weight: 700;
+  margin: 0;
+  line-height: 0.9;
 
     @media screen and (max-width: 768px) {
     font-weight: 600;
     padding-left: 20px;
     padding-top: 150px;
     line-height: none;
-
   }
 `;
 
-const Story = styled.h1`
-  color: #fff;
-  font-size: 55px;
-  padding-left: 60px;
-  padding-top: 30px;
-  font-weight: 600;
-  line-height: 1.1;
-  letter-spacing: 0.5px;
-  text-shadow: 0 0 20px #000;
-  margin-left: 50px;
-  background-color: rgba(0, 0, 0, 0.5);
-  border-radius: 40px;
-  width: 40%;
-  height: 30%;
-
-  @media screen and (max-width: 768px) {
-    font-size: 30px;
-    padding-left: 20px;
-    padding-top: 30px;
-    margin-left: 15px;
-    width: 80%;
-    height: 20%;
-  }
+const Promo = styled.div`
+width: 100%;
+height: 12%;
+display: flex;
+flex-direction: row;
+color: #000;
+background-color: red;
+margin-top: 20px;
 `;
+
+const UpTo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  margin-top: 8px;
+  width: 4%;
+  height: 70%;
+  padding-left: 10px;
+  // background-color: #d00000;
+`;
+
+const UpToText = styled.p`
+margin: 0;
+font-size: 14px;
+font-weight: 700;
+font-family: "Poppins";
+// line-height: 12px;
+// text-align: center;
+// justify-content: center;
+height: 50%;
+`;
+
+const PromoText = styled.h1`
+  font-size: 40px;
+  font-weight: 300;
+  font-family: "Poppins";
+  margin: 0;
+  `;
+
+  const PromoImg = styled.img`
+  width: 50%;
+  height: 100%;
+  object-fit: cover;
+  background-color: #d00000;
+  `; 
+
 
 const Button = styled.button`
   width: 120px;
@@ -80,12 +138,22 @@ const Button = styled.button`
 const SectionOne = () => {
   return (
     <Container>
-      <Discount>Get Amazing Discount %</Discount>
+      {/* <Discount>Get Amazing Discount %</Discount> */}
       <Story>
-        Get Favorite Shoes & <br />
-        Enjoy Your Journey
-      </Story>
+        <StoryHeading>
+       <Discount>Classic</Discount>
+       <Discount color="#c39f51">Soles</Discount>
+        </StoryHeading>
+        <Promo>
+          <UpTo>
+            <UpToText>UP</UpToText>
+            <UpToText>TO</UpToText>
+          </UpTo>
+          <PromoText>5% Discount</PromoText>
+          <PromoImg src={SalesImg} alt="sales" />
+        </Promo>
       <Button>Shop Now</Button>
+      </Story>
     </Container>
   );
 };
--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -7,9 +7,9 @@ import Modal from "react-modal";
 import { BiLogOut } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 import ObaLogo from "../../Image/OBA SOLES 1.png";
-import { AiOutlineSearch } from 'react-icons/ai';
-import { FaAngleDown } from 'react-icons/fa';
-import { FaShoppingCart } from 'react-icons/fa';
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaAngleDown } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
 
 const fadeIn = keyframes`
   // from { opacity: 0; transform: translateY(-20px); }
@@ -26,34 +26,37 @@ const NewContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   text-align: center;
-  `;
+  position: sticky;
+  top: 10px;
+  z-index: 100;
+`;
 
-  const NavLogo = styled.div`
+const NavLogo = styled.div`
   width: 15%;
   height: 100%;
   display: flex;
   justify-content: center;
   animation: ${fadeIn} 0.8s ease forwards;
   animation-delay: 0.2s;
-  `;
+`;
 
-  const Logo = styled.img`
+const Logo = styled.img`
   width: 100%;
   height: 90%;
   object-fit: contain;
   margin-top: 3px;
-  `;
+`;
 
-  const NavLinkSerachBox = styled.div`
+const NavLinkSerachBox = styled.div`
   width: 60%;
   height: 100%;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  `;
+`;
 
-  const NavLinks = styled.div`
+const NavLinks = styled.div`
   display: flex;
   justify-content: space-between;
   list-style: none;
@@ -66,18 +69,17 @@ const NavItems = styled(NavLink)`
   font-family: "Poppins", sans-serif;
   font-size: 16px;
   font-weight: 700;
-  animation: ${fadeIn} 0.10s ease forwards;
-  animation-delay: ${props => props.index * 0.3}s;
-
+  animation: ${fadeIn} 0.1s ease forwards;
+  animation-delay: ${(props) => props.index * 0.3}s;
 
   &:hover {
     cursor: pointer;
     text-decoration: underline;
-    text-decoration-color: #C39F51;
+    text-decoration-color: #c39f51;
   }
   &.active {
-    color: #C39F51;
-    text-decoration-color: #C39F51;
+    color: #c39f51;
+    text-decoration-color: #c39f51;
     text-decoration: underline;
   }
 `;
@@ -91,9 +93,9 @@ const SearchBox = styled.div`
   position: relative;
   animation: ${fadeIn} 0.5s ease forwards;
   animation-delay: 0.4s;
-  `;
+`;
 
-  const NewSearch = styled.input`
+const NewSearch = styled.input`
   height: 35%;
   width: 100%;
   border-radius: 8px;
@@ -116,7 +118,7 @@ const AccountCart = styled.div`
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  `;
+`;
 
 const AccountInfo = styled.div`
   display: flex;
@@ -132,9 +134,9 @@ const AccountInfo = styled.div`
   &:hover {
     cursor: pointer;
   }
-  `;
+`;
 
-  const AccountText = styled.p`
+const AccountText = styled.p`
   font-size: 16px;
   font-weight: 700;
   height: 100%;
@@ -142,27 +144,27 @@ const AccountInfo = styled.div`
   align-items: center;
   margin: 0;
   padding-bottom: 5px;
-  `;
+`;
 
-  const CartBox = styled.div`
+const CartBox = styled.div`
   width: 25%;
   height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
   position: relative;
-  `;
+`;
 
-  const StyledFaShoppingCart = styled(FaShoppingCart)`
+const StyledFaShoppingCart = styled(FaShoppingCart)`
   height: 25px;
   width: 25px;
-  `;
+`;
 
-  const Count = styled.span`
+const Count = styled.span`
   position: absolute;
   top: 10%;
   right: 0;
-  background-color: #C39F51;
+  background-color: #c39f51;
   color: #fff;
   border-radius: 50%;
   padding: 7px;
@@ -172,19 +174,13 @@ const AccountInfo = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  `;
-
-
-
-
-
+`;
 
 const Navbar = () => {
   const navigate = useNavigate();
   const [cartCount, setCartCount] = useState(0);
   const { name, setName } = React.useContext(UserContext);
   const [isModalOpen, setIsModalOpen] = useState(false);
-
 
   function openModal() {
     if (name) {
@@ -218,121 +214,44 @@ const Navbar = () => {
   }, []);
 
   return (
-    <>
     <NewContainer>
-    <NavLogo>
-      <Logo src={ObaLogo} alt="Oba Logo" />
-    </NavLogo>
-    <NavLinkSerachBox><NavLinks>
-          <NavItems to="/" exact>
-            Home
-          </NavItems>
-          <NavItems index={0} to="/product">Product</NavItems>
-          <NavItems index={1}to="/about">About</NavItems>
-          <NavItems index={2}to="/contact">Contact</NavItems>
-          <NavItems index={3}to="/blog">Blog</NavItems>
-        
-        </NavLinks>
-          <SearchBox>
-          <NewSearch type="search" />
-          <StyledSearchIcon />
-          </SearchBox></NavLinkSerachBox>
-    
-          <AccountCart>
-          <AccountInfo>
-            <AccountText>My Account</AccountText>
-            <FaAngleDown />
-          </AccountInfo>
-          <CartBox>
-          <StyledFaShoppingCart/>
-          <Count>{cartCount}</Count>
-          </CartBox>
-          </AccountCart>
-    </NewContainer>
-
-
-
-      {/* <NavContainer>
-        <BrandName>
-          Oba<Sole>Soles</Sole>
-        </BrandName>
-
+      <NavLogo>
+        <Logo src={ObaLogo} alt="Oba Logo" />
+      </NavLogo>
+      <NavLinkSerachBox>
         <NavLinks>
           <NavItems to="/" exact>
             Home
           </NavItems>
-          <NavItems to="/product">Product</NavItems>
-          <NavItems to="/about">About</NavItems>
-          <NavItems to="/contact">Contact</NavItems>
-          <NavItems to="/blog">Blog</NavItems>
+          <NavItems index={0} to="/product">
+            Product
+          </NavItems>
+          <NavItems index={1} to="/about">
+            About
+          </NavItems>
+          <NavItems index={2} to="/contact">
+            Contact
+          </NavItems>
+          <NavItems index={3} to="/blog">
+            Blog
+          </NavItems>
         </NavLinks>
-        <Search type="search" placeholder="Search for product" />
-        <CartContainer>
-          <Link to="/login">
-            <UserImg onClick={openModal}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <UserName>{name}</UserName>
-            </UserImg>
-          </Link>
-          <FontAwesomeIcon
-            icon={faShoppingCart}
-            style={{ fontSize: "23px", paddingBottom: "5px" }}
-          />
-          <CartCount>{cartCount}</CartCount>
-        </CartContainer>
-      </NavContainer>
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-        contentLabel="User Modal"
-        style={{
-          content: {
-            width: "250px",
-            height: "350px",
-            margin: "auto",
-            // position: "absolute", // Add this line
-            // top: "5px", // Adjust this value as needed
-            // left: "1000px",
-            borderRadius: "20px",
-            border: " 3px solid #ee0000",
-          },
-          overlay: {
-            backgroundColor: "rgba(0, 0, 0, 0.85)", // This is black with 75% opacity
-          },
-        }}
-      >
-        <div style={{ position: "absolute", top: "10px", right: "10px" }}>
-          <MdClose onClick={closeModal} size={24} color="black" />
-        </div>
-        <h2>Hello, {name}</h2>
-        <BreakPoint />
-        <div onClick={goHome}>
-          <MdHome size={24} color="#ee0000" />
-          <SpanText>Home</SpanText>
-        </div>
-        <BreakPoint />
-        <div>
-          <MdEdit size={24} color="#ee0000" />
-          <SpanText>Edit Profile</SpanText>
-        </div>
-        <BreakPoint />
-        <div onClick={handleLogout}>
-          <BiLogOut size={24} color="#ee0000" />
-          <SpanText>Logout</SpanText>
-        </div>
-      </Modal> */}
-    </>
+        <SearchBox>
+          <NewSearch type="search" />
+          <StyledSearchIcon />
+        </SearchBox>
+      </NavLinkSerachBox>
+      <AccountCart>
+        <AccountInfo>
+          <AccountText>My Account</AccountText>
+          <FaAngleDown />
+        </AccountInfo>
+        <CartBox>
+          <StyledFaShoppingCart />
+          <Count>{cartCount}</Count>
+        </CartBox>
+      </AccountCart>
+    </NewContainer>
   );
 };
 

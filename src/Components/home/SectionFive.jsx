084085fcfdaf5a2ee51c26e66033bb9f082f2bed
oluwagbogo <oluwagbogoadebanjo@gmail.com>
2024-04-24 23:58:39 +0100

    Sec 5 up

--- src/Components/home/SectionFive.jsx
+++ src/Components/home/SectionFive.jsx
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
+import MenFoot from '../../Image/italian-man-style (1).png'
 
 const Container = styled.div`
   height: 55%;
   width: 100%;
+  background-image: src(${MenFoot});
+  background-size: cover;
   `;
 const SectionFive = () => {
   return (
-    <div>SectionFive</div>
+    <Container>SectionFive</Container>
   )
 }
 

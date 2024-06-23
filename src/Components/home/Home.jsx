ba2182aa49c21a4d2cee148349b5b2330c371025
oluwagbogo <oluwagbogoadebanjo@gmail.com>
2024-06-23 23:58:23 +0100

    RecentProduvts

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -5,10 +5,13 @@ import SectionFour from "./SectionFour";
 import FashionBlog from "./FashionBlog";
 import Section3 from "./Section3";
 import SectionFive from "./SectionFive";
+import RecentProduct from "./RecentProduct";
+
 const Home = () => {
   return (
     <>
       <SectionOne/>
+      {/* <RecentProduct/> */}
       <SectionTwo/>
       <Section3/>
       <SectionFour/>
---
+++ src/Components/home/RecentProduct.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    height: 40vh;
+    width: 100%;
+    background-color: #f7f7f7;
+    `;
+
+function RecentProduct() {
+  return (
+    <Container>RecentProduct</Container>
+  )
+}
+
+export default RecentProduct
